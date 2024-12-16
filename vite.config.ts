c3ff35550c5f610af9c00a6c6159f66d4031a920
Diogo Moretti <diogo.pearljam@gmail.com>
2024-12-16 09:58:40 -0300

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,36 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
+import { resolve } from 'path';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+  plugins: [
+    react(),
+    dts({ include: ['src/hooks'] })
+  ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@components': path.resolve(__dirname, './src/components'),
-      '@hooks': path.resolve(__dirname, './src/hooks'),
-      '@styles': path.resolve(__dirname, './src/styles')
+      '@': resolve(__dirname, './src'),
+      '@components': resolve(__dirname, './src/components'),
+      '@hooks': resolve(__dirname, './src/hooks'),
+      '@styles': resolve(__dirname, './src/styles')
     },
   },
+  build: {
+    copyPublicDir: false,
+    lib: {
+      entry: resolve(__dirname, 'src/hooks/useDoubleClick.ts'),
+      formats: ['es', 'cjs'],
+      fileName: (format) => `index.${format === 'es' ? 'js' : 'cjs'}`
+    },
+    rollupOptions: {
+      external: ['react', 'react/jsx-runtime', 'double-click'],
+      output: {
+        globals: {
+          react: 'React',
+          doubleClick: 'DoubleClick'
+        }
+      }
+    }
+  }
 });
